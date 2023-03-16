87a0688ce0947f96b6e400eb8b99dad51872339f
Diego Rivera <99501856+DiegoRiv1@users.noreply.github.com>
2023-03-16 12:47:12 -0600

    Add files via upload

---
+++ EVA2_13_ABSTRACTAS_HERENCIA/src/eva2_13_abstractas_herencia/Figuras.java
@@ -0,0 +1,12 @@
+package eva2_13_abstractas_herencia;
+
+/**
+ *
+ * @author Diego
+ */
+
+//No sirve para crear objetos, solo como superClase
+public abstract class Figuras {
+    public abstract double calcularArea();
+    public abstract double calcularPerimetro();
+}
